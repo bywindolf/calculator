0e82edf37c962db244907d60413c3690a9a17779
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-01-10 14:31:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let calculation = ''; // Holds the sum/result
+
+console.log('hej');
+
+function addToCalculation(number) {
+    calculation += number;
+    updateResult(calculation);
+}
+
+function updateResult(value){
+document.querySelector('.result').textContent = value;
+}
